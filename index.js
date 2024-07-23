1a632e399f0c54b5e984d7b3e4142fe3107a3dc2
Soumajit Roy <itssoumajit.alt@gmail.com>
2024-07-24 02:00:34 +0530

    first progress

---
+++ index.js
@@ -0,0 +1,112 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let pwordEl1 = document.getElementById("pword1");
+let pwordEl2 = document.getElementById("pword2");
+
+//For generating random numbers
+function randomNum() {
+  let num = Math.floor(Math.random() * characters.length);
+  return num;
+}
+
+//main generating function
+function generatePword() {
+  for (let i = 1; i < 16; i += 1) {
+    pwordEl1.textContent += characters[randomNum()];
+  }
+  for (let i = 1; i < 16; i += 1) {
+    pwordEl2.textContent += characters[randomNum()];
+  }
+}
